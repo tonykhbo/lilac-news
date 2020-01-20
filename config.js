6193ad5b083bd7a1aa664ebd86fdae47e56d8bc4
tonykhbo <tonybo@live.unc.edu>
2020-01-20 12:13:14 -0500

    set to development

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //require('dotenv').config()
 
 const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'development'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
